19e7833bb43f41e85c5f11fb7d860f2e5c5d9380
subhadri <ssmallik1992@gmail.com>
2022-08-31 14:04:47 +0530

    Configure DCFValuation and plot-helper functions to automatically fetch dates from FinancialStatement

---
+++ finModel/analysis/ratios.py
@@ -0,0 +1,19 @@
+'''
+PURPOSE: Houses the ratios and datasets we would be using for plotting purposes
+'''
+
+from finModel.statements.main import FinancialStatement
+from finModel.statements.income import IncomeStatement
+import pandas as pd
+
+
+def ebitda_revenue_ratio(fin:FinancialStatement) -> pd.DataFrame:
+
+    ebitda: pd.Series = fin.income.ebitda
+    revenue: pd.Series = fin.income.revenue.revenue
+    ratio: pd.Series = (ebitda/revenue) * 100
+    combined: pd.DataFrame = pd.concat([revenue,ebitda,ratio],axis=1)
+    combined.columns = ["Revenues","EBITDA","EBITDA%"]
+    combined["period"] = combined.index
+
+    return combined.reset_index()
