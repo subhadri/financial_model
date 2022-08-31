749f8ee6f906b8fcae1cceb9bdd82bd4d1d5b88e
subhadri <ssmallik1992@gmail.com>
2022-08-31 13:57:42 +0530

    Add actual and forecast dates as attributes to FInancialStatement class

--- finModel/statements/main.py
+++ finModel/statements/main.py
@@ -1,26 +1,31 @@
-import pandas as pd
-
 from finModel.statements.forecast import is_forecast_avg_growth, bs_forecast_avg_growth
 from finModel.statements.income import IncomeStatement
 from finModel.statements.balance import BalanceSheet
 from finModel.statements.cashflow import CashFlowStatement
 from dataclasses import dataclass, field
 from typing import List, Union
+import pandas as pd
+import numpy as np
 
 @dataclass
 class FinancialStatement:
     company: str
+    actual_dates: List[str]
+    forecast_dates: List[str]
     income: IncomeStatement = field(init=False)
     balance: BalanceSheet = field(init=False)
     cash: CashFlowStatement = field(init=False)
 
-    def __init__(self, comp:str, f_date:List[str], inc:IncomeStatement, bs:BalanceSheet):
+    def __init__(self, comp:str, a_date:List[str], f_date:List[str], inc:IncomeStatement, bs:BalanceSheet):
         self.company = comp
+        self.actual_dates = a_date
+        self.forecast_dates = f_date
         f_inc = is_forecast_avg_growth(inc,f_date)
         f_bs = bs_forecast_avg_growth(bs,inc,f_inc,f_date)
         self.income = inc.attach(f_inc)
         self.balance = bs.attach(f_bs)
         self.cash = CashFlowStatement(self.income,self.balance)
+
 
 def report_table(d:Union[IncomeStatement,BalanceSheet,CashFlowStatement],f_date:List[str]) -> pd.DataFrame:
 
